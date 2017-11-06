aaaba37ad6877afba07f706c72dd9d9d81513e06
aniruddha <aniruddha.kudalkar@gmail.com>
2017-11-06 11:26:01 +0530

    modules and resources

---
+++ androlib/src/main/java/com/codekul/androlib/Setup.java
@@ -0,0 +1,12 @@
+package com.codekul.androlib;
+
+/**
+ * Created by aniruddha on 6/11/17.
+ */
+
+public class Setup {
+
+    public void apkSetup() {
+
+    }
+}
--- app/src/main/java/com/codekul/androidnsnov101130/MainActivity.kt
+++ app/src/main/java/com/codekul/androidnsnov101130/MainActivity.kt
@@ -2,11 +2,15 @@ package com.codekul.androidnsnov101130
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.codekul.androlib.Setup
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val stp : Setup = Setup()
+        stp.apkSetup()
     }
 }
