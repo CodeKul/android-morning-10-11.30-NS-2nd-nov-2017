e0ae053b4254ab50a9e5b743f8a37b796bdc0aed
aniruddha <aniruddha.kudalkar@gmail.com>
2017-11-15 11:06:30 +0530

    animation : view animations

---
+++ viewanimations/src/main/java/com/codekul/viewanimations/MainActivity.java
@@ -0,0 +1,21 @@
+package com.codekul.viewanimations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onAnimate(View view) {
+        Animation anm = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        findViewById(R.id.imageView).startAnimation(anm);
+    }
+}
